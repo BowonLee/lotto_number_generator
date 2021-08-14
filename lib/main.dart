cb560e1b7f3b30c644557b87d88b5a31f4dbb413
bowon lee <asdfg3678@gmail.com>
2021-08-14 12:38:12 +0900

    텍스트만 표시 가능하도록 적용

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,15 @@ import 'package:lotto_number_generator/view/LottoNumberGenerator.dart';
 void main() {
   runApp(LottoNumberGenerator());
 }
+
+
+class AppBuilder extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    String _title = "Lotto!";
+    return MaterialApp(
+      title: _title,
+      home: LottoNumberGenerator(),
+    );
+  }}
--- lib/view/LottoNumberGenerator.dart
+++ lib/view/LottoNumberGenerator.dart
@@ -1,15 +1,18 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lotto_number_generator/model/NumberGenerator.dart';
 
 class LottoNumberGenerator extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
+    NumberGenerator numberGenerator = new NumberGenerator();
     return Scaffold(
       appBar: AppBar(
         title: Text("Please God....."),
       ),
+      body: Text(numberGenerator.toString()),
     );
   }
 }
